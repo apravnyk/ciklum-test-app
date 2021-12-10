5673ee5fd7393c1808c61b245b466188918b257c
apravnyk <pravnyka@gmail.com>
2021-12-10 10:24:47 +0200

    Added logic for favorite film

--- src/components/FilmDetails.tsx
+++ src/components/FilmDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Descriptions } from 'antd';
+import { Checkbox, Descriptions } from 'antd';
 import { Film } from '../types';
+import { useFavoriteFilm } from '../hooks/useFavoriteFilm';
 
 export interface FilmDetailsProps {
   film: Film | null;
@@ -11,19 +12,25 @@ export const FilmDetails: React.FC<FilmDetailsProps> = (
 ) => {
   const { film } = props;
 
+  const { onChangeListFilms, isFavoriteFilm } = useFavoriteFilm();
+
   return (
-    <>
-      <Descriptions
-        title="Film Details"
-        bordered
-        column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }}
-      >
-        <Descriptions.Item label="Title">{film?.title}</Descriptions.Item>
-        <Descriptions.Item label="Description">
-          {film?.description}
-        </Descriptions.Item>
-        <Descriptions.Item label="Producer">{film?.producer}</Descriptions.Item>
-      </Descriptions>
-    </>
+    <Descriptions
+      title="Film Details"
+      bordered
+      column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }}
+    >
+      <Descriptions.Item label="Title">{film?.title}</Descriptions.Item>
+      <Descriptions.Item label="Description">
+        {film?.description}
+      </Descriptions.Item>
+      <Descriptions.Item label="Producer">{film?.producer}</Descriptions.Item>
+      <Descriptions.Item label="Add to favorite">
+        <Checkbox
+          onChange={(e) => onChangeListFilms(film?.id, e.target.checked)}
+          checked={isFavoriteFilm(film?.id)}
+        />
+      </Descriptions.Item>
+    </Descriptions>
   );
 };
